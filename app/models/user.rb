48cd5acc0ad0e09328037c65c3943f736eb7afd0
takuzoo0 <81349515+takuzoo0@users.noreply.github.com>
2021-04-27 22:58:41 +0900

    userモデルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :products
+  has_many :buyers
+
+  with_options presence: true do
+    validates :nickname
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+    validates :birth_day
+  end
 end
