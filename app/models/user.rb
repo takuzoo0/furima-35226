a588a4170f43956ccab7b155bfd6742a87011502
takuzoo0 <81349515+takuzoo0@users.noreply.github.com>
2021-04-28 16:42:46 +0900

    userモデルにてバリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,16 @@ class User < ApplicationRecord
     validates :last_name_kana
     validates :birth_day
   end
+
+  validates :encrypted_password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/} do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
 end
