3345606f82c1a961d7378aff1127df597c355746
takuzoo0 <81349515+takuzoo0@users.noreply.github.com>
2021-05-02 17:40:43 +0900

    productモデルの各カラムが空では保存できないバリデーションの記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,15 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_one :buyer
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :ship_to_day_id
+    validates :price
+  end
 end
