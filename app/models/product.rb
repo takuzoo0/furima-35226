2ce7f2aa8fe2da3689f8d9e9f4f79d76ef60bf6a
takuzoo0 <81349515+takuzoo0@users.noreply.github.com>
2021-05-03 00:43:56 +0900

    ActiveHashの実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture_id
+  belongs_to :shipping_charge
+  belongs_to :ship_to_day
   belongs_to :user
   has_one :buyer
   has_one_attached :image
@@ -13,4 +19,11 @@ class Product < ApplicationRecord
     validates :ship_to_day_id
     validates :price
   end
+
+  with_options numericality: { other_than: 1 }
+  validates :category_id
+  validates :condition_id
+  validates :shipping_charge_id
+  validates :prefecture_id
+  validates :ship_to_day
 end
