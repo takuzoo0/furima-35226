f24c12b5c3fddc5c504089ec785cd2de91e538c9
takuzoo0 <81349515+takuzoo0@users.noreply.github.com>
2021-05-02 19:31:59 +0900

    ActiveStorageの導入

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,4 +1,10 @@
 class ProductController < ApplicationController
   def index
   end
+
+  private
+
+  def message_params
+    params.require(:product).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :ship_to_day_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_one :buyer
+  has_one_attached :image
 
   with_options presence: true do
     validates :name
