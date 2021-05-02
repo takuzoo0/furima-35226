ef31eb7945445bb28da5f2a2099ea61d3d5cbfbd
takuzoo0 <81349515+takuzoo0@users.noreply.github.com>
2021-05-03 01:30:40 +0900

    productモデルのバリデーションの修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,14 +10,10 @@ class Product < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :description
-    validates :category_id
-    validates :condition_id
-    validates :shipping_charge_id
-    validates :prefecture_id
-    validates :ship_to_day_id
-    validates :price
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: /\A[0-9]+\z/ }
   end
 
   with_options numericality: { other_than: 1 }
