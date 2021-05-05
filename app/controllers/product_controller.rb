d82995ef2980d42d77187cf1886f3f011721f505
takuzoo0 <81349515+takuzoo0@users.noreply.github.com>
2021-05-05 18:52:43 +0900

    商品編入機能の実装

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,5 +1,6 @@
 class ProductController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+
   def index
     @products = Product.all.order(created_at: :desc)
   end
@@ -21,9 +22,31 @@ class ProductController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    move_to_index
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    move_to_index
+    if @product.update(product_params)
+      redirect_to product_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :ship_to_day_id, :price).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless current_user == @product.user
+      redirect_to root_path
+    end
+  end
+
 end
